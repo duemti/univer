ca113d353cdf430b57d6fdcb247531dd290308f8
dp97 <petrov.dumitru@gmail.com>
2018-05-22 14:03:19 +0300

    all done

--- graphics/l3.c
+++ graphics/l3.c
@@ -1,35 +1,35 @@
 /**********************/
 /* By. Dumitru Petrov */
-/* Lab. 2 p.2 V21     */
+/* Lab. 3 p.2 V21     */
 /**********************/
 
 #include <graphics.h>
 #include <math.h>
 #include <X11/Xlib.h>
 
-#define G GREEN
-
+/*
+**	Conversion of x coordinate - Window to Viewport
+*/
 double	transformx(double coor, int *w)
 {
-	double s, t;
-
+	if (!w)
+		return coor;
 	return (((coor - w[0]) / (w[2] - w[0])) * (w[6] - w[4])) + w[4];
-
-	s = ((w[6] - w[4]) / (w[2] - w[0]));
-	t = w[4] - w[0] * s;
-	return (coor * s + t);
 }
+/*
+**	Conversion of y coordinate - Window to Viewport
+*/
 double	transformy(double coor, int *w)
 {
-	double s, t;
-
+	if (!w)
+		return coor;
 	return (((coor - w[1]) / (w[3] - w[1])) * (w[7] - w[5])) + w[5];
-
-	s = (w[7] - w[5]) / (w[3] - w[1]);
-	t = w[5] - s * w[1];
-	return (coor * s + t);
 }
 
+/*
+**	draw "y = 5 * x - 3 * sin(pow(x, 2) * (k * x))" graphic
+** 	if window parameter is null, then it is displayed on whole window
+*/
 void	draw_first(int x_axis, int y_axis, double k, int window[])
 {
 	double x = -x_axis, y, px, py, cx = getmaxx()/2, cy = getmaxy()/2;
@@ -56,13 +56,17 @@ void	draw_first(int x_axis, int y_axis, double k, int window[])
 			lineto(px, py);
 			/* update CP */
 			moveto(px, py);
-		}
+		} else {flag = 1;}
 
 		x += 0.01;
 		delay(5);
 	}
 }
 
+/*
+**	draw "y = cos(k * x)" graphic
+** 	if window parameter is null, then it is displayed on whole window
+*/
 void	draw_second(int x_axis, int y_axis, double k, int *window)
 {
 	double x = -x_axis, y, px, py, cx = getmaxx()/2, cy = getmaxy()/2;
@@ -92,6 +96,10 @@ void	draw_second(int x_axis, int y_axis, double k, int *window)
 	}
 }
 
+/*
+**	draw "y = 4 * pow(x, 7) - 3 * pow(x, 3) + 5" graphic
+** 	if window parameter is null, then it is displayed on whole window
+*/
 void	draw_last(int x_axis, int y_axis, float direction, int window[])
 {
 	double x = 0, y, px, py, cx = getmaxx()/2, cy = getmaxy()/2;
@@ -144,15 +152,17 @@ XInitThreads();
 	int window2[] = {0, 0, getmaxx(), getmaxy(), getmaxx()/2, 0, getmaxx(), getmaxy()/2};
 	int window3[] = {0, 0, getmaxx(), getmaxy(), 0, getmaxy()/2, getmaxx(), getmaxy()};
 
+	/* I displayed them in this order because of better looking */
 	rectangle(getmaxx()/2, 0, getmaxx(), getmaxy()/2);
 	draw_axis("-4", "4", "12", "-12", window2);
 	setcolor(GREEN);
 	setlinestyle(SOLID_LINE, 0, 2);
-	draw_first(5, 12, 2, window2);
+	draw_first(5, 12, 1, window2);
 	
 	setcolor(WHITE);
 	setlinestyle(SOLID_LINE, 0, 1);
 	rectangle(0, 0, getmaxx()/2, getmaxy()/2);
+	
 	draw_axis("-2", "2", "7", "-7", window1);
 	setcolor(MAGENTA);
 	setlinestyle(SOLID_LINE, 0, 2);
@@ -164,10 +174,11 @@ XInitThreads();
 	setcolor(WHITE);
 	setlinestyle(SOLID_LINE, 0, 1);
 	rectangle(0, 0, getmaxx()/2, getmaxy()/2);
+
 	draw_axis("-6", "6", "2", "-2", window3);
 	setcolor(RED);
 	setlinestyle(SOLID_LINE, 0, 2);
-	draw_second(6, 2, 2, window3);
+	draw_second(6, 2, 1, window3);
 
 	getch();
 	closegraph();
